54e27ff340141de3590b4a02fec4f92be83cdbf1
xLisu <pocztalis@icloud.com>
2024-03-21 17:56:10 +0000

    solution

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ class SilverPoint:
         self.score = score
 
 
-def tile_direction():
-    {
+class MapPoint:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.directions = [direction]
+
+
+def tile_direction(direction):
+    tile_id_to_dir = {
         "3": "LR",
         "5": "DR",
         "6": "LD",
@@ -35,21 +42,36 @@ def tile_direction():
         "E": "LU LD UD",
         "F": "LR LD LU UD DR UR",
     }
-    {
-        "LR": ["3", "7", "B", "C3", "F"],
-        "UD": ["C", "C3", "D", "E", "F"],
-        "LU": ["A", "A5", "B", "E", "F"],
-        "LD": ["6", "7", "96", "E", "F"],
-        "UR": ["9", "96", "B", "D", "F"],
-        "DR": ["5", "7", "A5", "D", "F"],
+    dir_to_tile_ids = {
+        # "LR": ["3", "7", "B", "C3", "F"],
+        # "UD": ["C", "C3", "D", "E", "F"],
+        # "LU": ["A", "A5", "B", "E", "F"],
+        # "LD": ["6", "7", "96", "E", "F"],
+        # "UR": ["9", "96", "B", "D", "F"],
+        # "DR": ["5", "7", "A5", "D", "F"],
+        # #Same but opposite
+        # "RL": ["3", "7", "B", "C3", "F"],
+        # "DU": ["C", "C3", "D", "E", "F"],
+        # "UL": ["A", "A5", "B", "E", "F"],
+        # "DL": ["6", "7", "96", "E", "F"],
+        # "RU": ["9", "96", "B", "D", "F"],
+        # "RD": ["5", "7", "A5", "D", "F"],
+        "LR": {"3", "7", "B", "F"},
+        "UD": {"C", "D", "E", "F"},
+        "LU": {"A", "B", "E", "F"},
+        "LD": {"6", "7", "E", "F"},
+        "UR": {"9", "B", "D", "F"},
+        "DR": {"5", "7", "D", "F"},
         #Same but opposite
-        "RL": ["3", "7", "B", "C3", "F"],
-        "DU": ["C", "C3", "D", "E", "F"],
-        "UL": ["A", "A5", "B", "E", "F"],
-        "DL": ["6", "7", "96", "E", "F"],
-        "RU": ["9", "96", "B", "D", "F"],
-        "RD": ["5", "7", "A5", "D", "F"],
+        "RL": {"3", "7", "B", "F"},
+        "DU": {"C", "D", "E", "F"},
+        "UL": {"A", "B", "E", "F"},
+        "DL": {"6", "7", "E", "F"},
+        "RU": {"9", "B", "D", "F"},
+        "RD": {"5", "7", "D", "F"},
     }
+
+    return dir_to_tile_ids[direction]
 
 
 def parse_data(file_name):
@@ -92,19 +114,92 @@ def parse_data(file_name):
 
     return W, H, Gn, Sm, Tl, golden_points, silver_points, tiles
 
+
 def output_results(file_name):
      with open(f'out_{file_name}.txt', mode='w') as file:
         for row in data_solution:
-            file.write(row + '\n')
-        pass
+            file.write(f"{row.x} {row.y} {row.directions[0]}\n")
 
+
+def find_common_elements(sets):
+    common_elements = sets[0]  # Start with the first set
+
+    for s in sets[1:]:
+        common_elements = common_elements.intersection(s)
+
+    return common_elements
+
+
+def calculate_middle_point():
+    # Calculate the total sum of x-coordinates and y-coordinates
+    total_x = sum(point.x for point in golden_points)
+    total_y = sum(point.y for point in golden_points)
+
+    # Calculate the average (mean) of x-coordinates and y-coordinates
+    middle_x = total_x / len(golden_points)
+    middle_y = total_y / len(golden_points)
+
+    return middle_x, middle_y
+
+
+def add_to_grid(grid, x, y, direction):
+    if grid[x][y] is None:
+        grid[x][y] = MapPoint(x, y, direction)
+        map_tiles.append(grid[x][y])
+    else:
+        grid[x][y].directions.append(direction)
 
 def solution():
-    results = []
-    for i in range(W):
-        for j in range(H):
-            pass
-    return results
+    middle_x, middle_y = calculate_middle_point()
+    grid = [[None for _ in range(W)] for _ in range(H)]
+    print(f"Middle Point: x:{middle_x}|y:{middle_y}")
+    for golden_point in golden_points:
+        x_diff = golden_point.x - middle_x
+        y_diff = golden_point.y - middle_y
+        current_x = golden_point.x
+        current_y = golden_point.y
+        while x_diff != 0:
+            if x_diff < 0:
+                # From left to right
+                x_diff += 1
+                current_x += 1
+                add_to_grid(grid, current_x, current_y, "LR")
+            else:
+                # From right to left
+                x_diff -= 1
+                current_x -= 1
+                add_to_grid(grid, current_x, current_y, "RL")
+        while y_diff != 0:
+            if y_diff > 0:
+                # From down to up
+                y_diff -= 1
+                current_y -= 1
+                add_to_grid(grid, current_x, current_y, "DU")
+            else:
+                # From up to down
+                y_diff += 1
+                current_y += 1
+                add_to_grid(grid, current_x, current_y, "UD")
+
+    tile_placement = []
+    for route in map_tiles:
+        if len(route.directions) == 1:
+            common_tile_ids = tile_direction(route.directions[0])
+            for tile_id_c in common_tile_ids:
+                if tiles[tile_id_c].num_of_tiles > 0:
+                    tile_placement.append(MapPoint(route.x, route.y, tile_id_c))
+                    tiles[tile_id_c].num_of_tiles -= 1
+        else:
+            list_of_dirs = []
+            for direction in route.directions:
+                list_of_dirs.append(tile_direction(direction))
+            common_tile_ids = find_common_elements(list_of_dirs)
+            for tile_id_c in common_tile_ids:
+                if tiles[tile_id_c].num_of_tiles > 0:
+                    tile_placement.append(MapPoint(route.x, route.y, tile_id_c))
+                    tiles[tile_id_c].num_of_tiles -= 1
+
+    return tile_placement
 
 
 if __name__ == "__main__":
@@ -126,6 +221,8 @@ if __name__ == "__main__":
         print(f"Silver Point x:{silver_point.x}|y:{silver_point.y}|score:{silver_point.score}")
     for tile_key, tile in tiles.items():
         print(f"Tile id:{tile_key}|cost:{tile.cost}|num:{tile.num_of_tiles}")
+
+    map_tiles = []
 
     data_solution = solution()
 
